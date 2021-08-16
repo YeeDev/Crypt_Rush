843fa6111a8a258b2463c4a71a6c460ad6362086
Yee <yee.gamedev@gmail.com>
2021-08-16 12:37:17 -0500

    Simple Camera Movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] float followSmooth = 1;
+
+    Vector3 velocity = Vector3.zero;
+    Transform player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, player.position, ref velocity, followSmooth);
+    }
+}
