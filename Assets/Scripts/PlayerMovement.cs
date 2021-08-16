7092d5a9582e12cfc708769a34e638d5552c93d7
Yee <yee.gamedev@gmail.com>
2021-08-16 12:17:37 -0500

    Player Movement and Color

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 3;
+    [SerializeField] float rotationSpeed = 40;
+
+    Vector3 moveDirection;
+    Rigidbody rgb;
+
+    void Awake()
+    {
+        rgb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        SetMovingDirection();
+        RotateCharacter();
+    }
+
+    private void FixedUpdate()
+    {
+        moveDirection.y = rgb.velocity.y;
+        rgb.velocity = moveDirection.normalized * moveSpeed;
+    }
+
+    private void RotateCharacter()
+    {
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            if (Equals(moveDirection, Vector3.zero)) { return; }
+
+            transform.rotation =
+                Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection), Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    private void SetMovingDirection()
+    {
+        moveDirection.x = Input.GetAxis("Horizontal");
+        moveDirection.z = Input.GetAxis("Vertical");
+    }
+}
