ba6c5772ba06dd7eca551cfc133560a6f7d88694
Yee <yee.gamedev@gmail.com>
2021-08-17 11:15:31 -0500

    Pushing Player When Hit

--- Assets/Scripts/Player SCR/HitTaker.cs
+++ Assets/Scripts/Player SCR/HitTaker.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 public class HitTaker : MonoBehaviour
 {
     [SerializeField] int hitsBeforeRestarting = 3;
+    [SerializeField] float pushForce = 10;
     [SerializeField] string[] damagerTags = null;
 
     bool isInvulnerable = false;
+    Rigidbody rgb;
     PlayerAnimator plyAnm;
 
     public bool IsAlive() { return hitsBeforeRestarting > 0; }
 
     private void Awake()
     {
+        rgb = GetComponent<Rigidbody>();
         plyAnm = GetComponent<PlayerAnimator>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (CheckForCorrectTag(collision.transform.tag) && !isInvulnerable) { CalculateHits(); }
+        if (CheckForCorrectTag(collision.transform.tag) && !isInvulnerable) { CalculateHits(collision.transform.position); }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (CheckForCorrectTag(other.tag) && !isInvulnerable) { CalculateHits(); }
+        if (CheckForCorrectTag(other.tag) && !isInvulnerable) { CalculateHits(other.transform.position); }
     }
 
     private bool CheckForCorrectTag(string tagToCheck)
@@ -36,18 +39,26 @@ public class HitTaker : MonoBehaviour
         return false;
     }
 
-    private void CalculateHits()
+    private void CalculateHits(Vector3 hitterPosition)
     {
         hitsBeforeRestarting--;
 
         if (hitsBeforeRestarting > 0)
         {
+            Push(hitterPosition);
+
             SetVulnerability();
             plyAnm.TriggerAnimation("Invunerable");
             return;
         }
 
         KillPlayer();
+    }
+
+    private void Push(Vector3 hitterPosition)
+    {
+        hitterPosition.y = transform.position.y;
+        rgb.AddForce((transform.position - hitterPosition).normalized * pushForce, ForceMode.VelocityChange);
     }
 
     //Also called in animation event.
