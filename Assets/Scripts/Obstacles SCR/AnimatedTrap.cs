efbdad7abb5521b06152da7a6111aea3e35e0b40
Yee <yee.gamedev@gmail.com>
2021-08-17 10:41:21 -0500

    Spinner Added

---
+++ Assets/Scripts/Obstacles SCR/AnimatedTrap.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedTrap : MonoBehaviour
+{
+    [Range(0, 10)][SerializeField] float animationSpeed = 1;
+
+    Animator anm;
+
+    private void Awake()
+    {
+        anm = GetComponent<Animator>();
+        anm.speed = animationSpeed;
+    }
+}
