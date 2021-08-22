095f8cb1ef8f84248e78e8b2bb3da9011df42cdf
Yee <yee.gamedev@gmail.com>
2021-08-22 10:41:21 -0500

    Level 19 and Added Offset Functionality

--- Assets/Scripts/Obstacle Mnt SCR/Obstacles SCR/AnimatedTrap.cs
+++ Assets/Scripts/Obstacle Mnt SCR/Obstacles SCR/AnimatedTrap.cs
@@ -5,13 +5,16 @@ namespace CryptRush.Obstacle
     public class AnimatedTrap : MonoBehaviour
     {
         [Range(0, 10)] [SerializeField] float animationSpeed = 1;
+        [Range(0, 1)] [SerializeField] float animationOffset = 0;
 
         Animator anm;
 
         private void Awake()
         {
             anm = GetComponent<Animator>();
+
             anm.speed = animationSpeed;
+            anm.SetFloat("Offset", animationOffset);
         }
     }
 }
