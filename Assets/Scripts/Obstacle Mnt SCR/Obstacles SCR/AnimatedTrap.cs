e54779c07ad9db94a97b6fb24a49cec1febeb71c
Yee <yee.gamedev@gmail.com>
2021-09-13 20:09:56 -0500

    Level 4 Terrain Art Added

--- Assets/Scripts/Obstacle Mnt SCR/Obstacles SCR/AnimatedTrap.cs
+++ Assets/Scripts/Obstacle Mnt SCR/Obstacles SCR/AnimatedTrap.cs
@@ -6,6 +6,7 @@ namespace CryptRush.Obstacle
     {
         [Range(0, 10)] [SerializeField] float animationSpeed = 1;
         [Range(0, 1)] [SerializeField] float animationOffset = 0;
+        [Range(-1, 1)] [SerializeField] int rotateDirection = 1;
 
         Animator anm;
 
@@ -20,6 +21,7 @@ namespace CryptRush.Obstacle
         {
             anm.speed = animationSpeed;
             anm.SetFloat("Offset", animationOffset);
+            anm.SetFloat("Direction", rotateDirection);
         }
     }
 }
