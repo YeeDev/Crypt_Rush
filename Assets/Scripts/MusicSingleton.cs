573b8d4e931f5b3efbda574d444eda20c38a287b
Yee <yee.gamedev@gmail.com>
2021-09-21 18:00:57 -0500

    Scythe Sounds Added

---
+++ Assets/Scripts/MusicSingleton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CryptRush.Sound
+{
+    public class MusicSingleton : MonoBehaviour
+    {
+        MusicSingleton musicPlayer;
+
+        public bool IsSingletonCreated { get => musicPlayer != null; }
+
+        private void Awake()
+        {
+            foreach (MusicSingleton musicPlayer in FindObjectsOfType<MusicSingleton>())
+            {
+                if (musicPlayer.IsSingletonCreated)
+                {
+                    Destroy(gameObject);
+                }
+            }
+
+            musicPlayer = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
