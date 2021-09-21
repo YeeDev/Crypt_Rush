ac810a7a73db75c296559fd47f992e0a2d03d28d
Yee <yee.gamedev@gmail.com>
2021-09-21 17:46:42 -0500

    Lots of Sounds Added... I should made smaller commits here, but who cares.

---
+++ Assets/Scripts/SFXPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CryptRush.Sound
+{
+    public class SFXPlayer : MonoBehaviour
+    {
+        [SerializeField] bool playsSound = true;
+        [SerializeField] AudioSource audioSource = null;
+        [SerializeField] bool setRandomOffset = true;
+
+        private void Awake()
+        {
+            if (setRandomOffset)
+            {
+                audioSource.timeSamples = (int)Random.Range(0, audioSource.clip.length);
+            }
+        }
+
+        public void PlaySound() { if (playsSound) { audioSource.Play(); } }
+    }
+}
