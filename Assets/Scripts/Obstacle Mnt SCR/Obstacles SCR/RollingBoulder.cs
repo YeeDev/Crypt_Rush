653975c82638297d45f7e2796a10aceeb58364a9
Yee <yee.gamedev@gmail.com>
2022-03-04 12:17:33 -0600

    Final Commit - Pre Release

--- Assets/Scripts/Obstacle Mnt SCR/Obstacles SCR/RollingBoulder.cs
+++ Assets/Scripts/Obstacle Mnt SCR/Obstacles SCR/RollingBoulder.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using CryptRush.Core;
 
 namespace CryptRush.Obstacle
 {
@@ -8,10 +9,16 @@ namespace CryptRush.Obstacle
         [SerializeField] float maxYSpeed = 5;
         [SerializeField] Vector3 moveDirection = Vector3.zero;
 
+        bool stopped;
         Vector3 directionalSpeed;
         Rigidbody rgb;
+        CameraVFX cameraVFX;
 
-        private void Awake() { rgb = GetComponent<Rigidbody>(); }
+        private void Awake()
+        {
+            rgb = GetComponent<Rigidbody>();
+            cameraVFX = FindObjectOfType<CameraVFX>();
+        }
 
         private void Update() { RollBall(); }
 
@@ -20,6 +27,16 @@ namespace CryptRush.Obstacle
             directionalSpeed = moveDirection.normalized * speed;
             directionalSpeed.y = Mathf.Clamp(rgb.velocity.y, -maxYSpeed, maxYSpeed);
             rgb.velocity = directionalSpeed;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!stopped && other.CompareTag("Boulder Stopper"))
+            {
+                stopped = true;
+                GetComponent<AudioSource>().Stop();
+                StartCoroutine(cameraVFX.CameraShake(1, 0.1f));
+            }
         }
     }
 }
